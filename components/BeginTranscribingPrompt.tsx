d2e56fcd884231edf40e800c6ac2f4d6df8462b1
Nathan Walters <nwalters512@gmail.com>
2019-12-08 21:10:10 -0800

    Put data handling policy in modal

--- components/BeginTranscribingPrompt.tsx
+++ components/BeginTranscribingPrompt.tsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
+import Modal from "./Modal";
 
-const BeginTranscribingPrompt = ({ onStart }) => (
-  <div className="flex flex-col flex-grow items-center justify-center">
-    <Button rounded className="text-xl" onClick={() => onStart()}>
-      Begin transcribing
-    </Button>
-    <p className="text-gray-600 max-w-md p-4">
-      Transcription uses your browser's built-in speech recognition
-      capabilities. Audio data may be sent to a server for processing. Your data
-      may be subject to your browser's privacy policies.
-    </p>
-  </div>
-);
+const BeginTranscribingPrompt = ({ onStart }) => {
+  const [showDataHandlingModal, setShowDataHandlingModal] = useState(false);
+  return (
+    <div className="flex flex-col flex-grow items-center justify-center">
+      <Button rounded className="text-xl" onClick={() => onStart()}>
+        Begin transcribing
+      </Button>
+      <button
+        className="m-4 text-gray-600 hover:underline"
+        onClick={() => setShowDataHandlingModal(true)}
+      >
+        How is my audio data handled?
+      </button>
+      <Modal
+        title="Audio data handling"
+        show={showDataHandlingModal}
+        onHide={() => setShowDataHandlingModal(false)}
+      >
+        <p className="mb-2">
+          This site does not record or store any audio data itself.
+          Transcriptions are provided by features built into your web browser.
+          Audio may be transcribed on your device, or it may be sent to a server
+          for processing. See your browser's privacy policy for more
+          information.
+        </p>
+        <Button onClick={() => setShowDataHandlingModal(false)}>OK</Button>
+      </Modal>
+    </div>
+  );
+};
 
 export default BeginTranscribingPrompt;
