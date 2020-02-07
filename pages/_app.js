cfb6dfe9f05ea21b648fb2c9bdc5d8cbc1c62d86
Rishub Kumar <rishub@gmail.com>
2020-02-07 13:49:54 -0800

    allow for hiding navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import Nav from '../components/Nav';
 
-import '../styles/index.css'
+import '../styles/index.css';
 
-function MyApp({ Component, pageProps }) {
-  return <div className="flex flex-col bg-gray-900 w-screen h-screen">
-    <Nav />
-    <div className="flex flex-col bg-gray-900 flex-grow overflow-y-auto">
-      <Component {...pageProps} />
+function MyApp(props) {
+  const {
+    Component,
+    pageProps,
+    router: { query },
+  } = props;
+
+  return (
+    <div className="flex flex-col bg-gray-900 w-screen h-screen">
+      {query.navbar !== 'false' && <Nav />}
+      <div className="flex flex-col bg-gray-900 flex-grow overflow-y-auto">
+        <Component {...pageProps} />
+      </div>
     </div>
-  </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
