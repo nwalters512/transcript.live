91cae1373d24952169ada5b21721d88ce6545eaf
Nathan Walters <nwalters512@gmail.com>
2020-02-09 17:21:39 -0800

    Run prettier

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import Nav from '../components/Nav';
+import React from "react";
+import Nav from "../components/Nav";
 
-import '../styles/index.css';
+import "../styles/index.css";
 
 function MyApp(props) {
   const {
     Component,
     pageProps,
-    router: { query },
+    router: { query }
   } = props;
 
   return (
     <div className="flex flex-col bg-gray-900 w-screen h-screen">
-      {query.navbar !== 'false' && <Nav />}
+      {query.navbar !== "false" && <Nav />}
       <div className="flex flex-col bg-gray-900 flex-grow overflow-y-auto">
         <Component {...pageProps} />
       </div>
